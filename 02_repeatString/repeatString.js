9960fd5c518fbf61bfb3f8e101078e068dcd6463
draiguv <dcrauv13@gmail.com>
2022-11-30 20:56:01 +0800

    Do JS exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    return num > 0 ? string + repeatString(string, --num)
+        : num < 0 ? 'ERROR'
+        : '';
 };
 
 // Do not edit below this line
