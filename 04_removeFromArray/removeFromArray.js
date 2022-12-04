a331bc6558517d4923825ddbbd7e8fdae74bcf75
draiguv <dcrauv13@gmail.com>
2022-12-04 16:58:35 +0800

    Do JS exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...argsVal) {
+    let indexOfValue;
 
+    argsVal.forEach(i => {
+        indexOfValue = arr.indexOf(i);
+        if (indexOfValue != -1) {
+            arr.splice(indexOfValue, 1);
+        }
+    });
+    
+    return arr;
 };
 
 // Do not edit below this line
