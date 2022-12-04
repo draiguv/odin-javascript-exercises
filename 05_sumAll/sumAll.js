5d4803a2f4ec7ee38ad564e18f72f13b448d7b6b
draiguv <dcrauv13@gmail.com>
2022-12-04 18:19:23 +0800

    Do JS exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (isNaN(a) && isNaN(b)) {
+        return 'ERROR';
+    }
+    
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
 
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+
+    let total = 0;
+
+    [a, b] = a >= b ? [b, a] : [a, b];
+    
+    for (; a <= b; a++) {
+        total += a;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
